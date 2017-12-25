c5d9aed53e4c46b6b2f325be125b110e007c91f2
Ilya Durdyev <funbanji@gmail.com>
2017-12-25 23:30:54 +0300

    TorgCRMMainLayout developed

--- CRMBaseModule/torgcrmmain.cpp
+++ CRMBaseModule/torgcrmmain.cpp
@@ -1,6 +1,8 @@
 #include "torgcrmmain.h"
 #include "ui_torgcrmmain.h"
 #include <QDebug>
+#include <QLineEdit>
+#include <QPushButton>
 
 TorgCRMMain::TorgCRMMain(QWidget *parent) :
     QMainWindow(parent),
@@ -9,6 +11,16 @@ TorgCRMMain::TorgCRMMain(QWidget *parent) :
     GLobalObject *globalObject = GLobalObject::getInstance();
     qDebug() << "Token is: " << globalObject->getToken() << endl;
     ui->setupUi(this);
+
+    ui->leftMenuFrame->layout()->setAlignment(Qt::AlignTop);
+    ui->rightContentFrame->layout()->setAlignment(Qt::AlignTop);
+
+    QLineEdit *toolBarSearchInput = new QLineEdit;
+    QPushButton *searchToolbarBtn = new QPushButton;
+    searchToolbarBtn->setText(tr("Search"));
+
+    ui->searchToolBar->addWidget(toolBarSearchInput);
+    ui->searchToolBar->addWidget(searchToolbarBtn);
 }
 
 TorgCRMMain::~TorgCRMMain()
