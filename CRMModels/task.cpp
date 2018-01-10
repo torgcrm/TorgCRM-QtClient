5d2aed351b407846a48619851d1a797729dbfa57
Ilya Durdyev <funbanji@gmail.com>
2018-01-10 17:33:49 +0300

    Task CPP file with getter and setters was modified

--- CRMModels/task.cpp
+++ CRMModels/task.cpp
@@ -2,3 +2,53 @@
 
 using namespace CModels;
 
+
+QString Task::getTitle() const
+{
+    return title;
+}
+
+void Task::setTitle(const QString &value)
+{
+    title = value;
+}
+
+QDateTime Task::getBeginDate() const
+{
+    return beginDate;
+}
+
+void Task::setBeginDate(const QDateTime &value)
+{
+    beginDate = value;
+}
+
+QDateTime Task::getEndDate() const
+{
+    return endDate;
+}
+
+void Task::setEndDate(const QDateTime &value)
+{
+    endDate = value;
+}
+
+QString Task::getComment() const
+{
+    return comment;
+}
+
+void Task::setComment(const QString &value)
+{
+    comment = value;
+}
+
+QString Task::getType() const
+{
+    return type;
+}
+
+void Task::setType(const QString &value)
+{
+    type = value;
+}
